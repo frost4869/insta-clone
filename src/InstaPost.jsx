0200806c4949c257c3a8928bf75650de69868905
Frost <imperio4869@gmail.com>
2017-11-24 12:17:13 +0700

    add video post support

--- src/InstaPost.jsx
+++ src/InstaPost.jsx
@@ -2,10 +2,17 @@ import React, { Component } from 'react'
 
 export default class InstaPost extends Component {
     render() {
-        const {post} = this.props;
+        const { post } = this.props;
+        const media_content = post.type == 'video' ?
+            <video width="500" autoPlay>
+                <source src={post.videos.low_bandwidth.url} />
+            </video>
+            :
+            <img src={post.images.standard_resolution.url} />
+
         return (
             <div>
-                <img src={post.images.standard_resolution.url}/>
+                {media_content}
                 <p>{post.caption ? post.caption.text : '#nocaption'}</p>
                 <p>{post.likes.count}</p>
             </div>
